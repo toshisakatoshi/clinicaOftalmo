14150def6e68631914db1ae23ebc4fe7605afbcf
Toshi <xerigrafi@gmail.com>
2018-08-27 21:16:45 -0300

    index

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <title>Clinica Oftalmo</title>
         <link href="logo.css" type="text/css" rel="stylesheet" />
         <link href="estilo.css" type="text/css" rel="stylesheet" />
-        
+<!--        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">-->
     </head>
     <body>
         <?php
@@ -31,6 +31,18 @@
         <div id="container">
             <img src="Logo 1.jpg" id="logo" alt="Logo da empresa" />
         </div>
+<!--        <div id="myCarousel" class="carousel slide">
+
+                        <div class="carousel-inner">
+                            <div class="active item"><img src="img/imagem3.jpg"/></div>
+                            <div class="item"><img src="img/imagem1.jpg"/></div>
+                            <div class="item"><img src="img/imagem2.jpg"/></div>
+
+                        </div>
+
+                        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                    </div>-->
         
         
         
@@ -40,5 +52,9 @@
         <?php
             require_once 'rodape.php';
         ?>
+<!--          </script>
+
+        <script src="http://code.jquery.com/jquery-latest.js"></script>
+        <script src="js/bootstrap.min.js"></script>-->
     </body>
 </html>
