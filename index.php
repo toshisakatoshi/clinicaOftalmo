12ea094579e72bbeb60ca918b6c7a6b02437a3b5
Toshinobu Sakamoto <181410024@senacinformatica.br>
2018-08-27 21:10:21 -0300

    configuração do logo

--- index.php
+++ index.php
@@ -10,31 +10,40 @@
         <title>Clinica Oftalmo</title>
         <link href="logo.css" type="text/css" rel="stylesheet" />
         <link href="estilo.css" type="text/css" rel="stylesheet" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
         
     </head>
     <body>
         <?php
             require_once 'cabecalho.php';
         ?>
-        <form action="entrar.php" method="POST" >
-        <input type="text" 
-               placeholder="E-mail / CPF: "
-               name="login" />
         
-        <input type="password" 
-               placeholder="Senha: "
-               name="senha" />
+       
         
-        <input type="submit" value="Entrar" />
-            
-    </form>
-        <div id="container">
+    <form class="form-horizontal" action="entrar.php" method="POST">
+  <div class="control-group">
+    <label class="control-label" for="inputEmail">Email</label>
+    <div class="controls">
+      <input type="text" id="inputEmail" placeholder="E-mail / CPF: "
+               name="login" >
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="inputPassword">Senha</label>
+    <div class="controls">
+      <input type="password" id="inputPassword" placeholder="Senha: " name="senha">
+    </div>
+  </div>
+  <div class="control-group">
+    <div class="controls">      
+      <button type="submit" class="btn">Entrar</button>
+    </div>
+  </div>
+</form>                    
+        
+         <div id="container">
             <img src="Logo 1.jpg" id="logo" alt="Logo da empresa" />
         </div>
-        
-        
-        
-        
         
         
         <?php
